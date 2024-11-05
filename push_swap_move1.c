bc279e649170177a5bd7a542a23f5dc1990dcfd4
Alexandre Tomasi <atomasi@c2r5s4.42lausanne.ch>
2024-11-05 18:10:16 +0100

    refare les fonctions avec liste chaine

---
+++ push_swap_move1.c
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_move1.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atomasi <atomasi@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/05 14:47:15 by atomasi           #+#    #+#             */
+/*   Updated: 2024/11/05 17:58:43 by atomasi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		*swap(int *stack)
+{
+	int temp;
+	int i;
+	i = 0;
+	while (stack[i])
+	{
+		i++;
+	}
+	if (i >= 2)
+	{
+		temp = stack[0];
+		stack[0] = stack_a[1];
+		stack[1] = temp;
+	}
+	return (stack);
+}
+
+void	push_a(int **stack_a, int **stack_b)
+{
+	int ia;
+	int ib;
+
+	ia = 0;
+	ib = 0;
+	while (&stack_a[ia])
+		ia++;
+	while (&stack_b[ib])
+		ib++;
+	if (ib >= 1)
+	{
+		while (ia > 0)
+		{
+			&stack_a[ia + 1] = &stack_a[ia];
+			ia--;
+		}
+		&stack_a[0] = &stack_b[0];
+	}
+}
+
+void	push_b(int **stack_a, int **stack_b)
+{
+	int ia;
+	int ib;
+
+	ia = 0;
+	ib = 0;
+	while (&stack_a[ia])
+		ia++;
+	while (&stack_b[ib])
+		ib++;
+	if (ia >= 1)
+	{
+		while (ib > 0)
+		{
+			&stack_b[ib + 1] = &stack_b[ib];
+			ib--;
+		}
+		&stack_b[0] = &stack_a[0];
+	}
+}
+
+int		*rotate(int *stack)
+{
+	int	i;
+	int	temp;
+
+	i = 0;
+	temp = stack[0];
+	while (stack[i + 1])
+	{
+		stack[i] = stack[i + 1];
+		i++;
+	}
+	stack[i] = temp;
+	stack[i + 1] = NULL;
+	return (stack);
+}
+
+int		*reverse_rotate(int *stack)
+{
+	int	i;
+	int	temp;
+
+	i = 0;
+	while (stack[i])
+		i++;
+	temp = stack[i];
+	stack[i + 1] = NULL;
+	while ((i - 1) > 0)
+	{
+		stack[i] = stack[i - 1];
+		i--;
+	}
+	stack[i] = temp;
+	return (stack);
+}
