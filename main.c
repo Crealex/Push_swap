0ccc862651e138cccc6415d8d0d0d2616e219d82
Alexandre Tomasi <atomasi@c2r5s4.42lausanne.ch>
2024-11-07 11:27:21 +0100

    passe la strcit

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: atomasi <atomasi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 17:59:43 by atomasi           #+#    #+#             */
-/*   Updated: 2024/11/05 18:08:37 by atomasi          ###   ########.fr       */
+/*   Updated: 2024/11/07 09:32:06 by atomasi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,11 @@
 int main(int argc, int argv)
 {
 	int i;
+	t_list stack_a;
+	t_list stack_b;
 
 	i = 0;
-	while(i < argc)
+	while(i < (argc - 1))
 	{
 
 	}
