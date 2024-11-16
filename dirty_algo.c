f708186585995ceb6c7209745e388fae2a93c9fa
Alexandre <alexandre.tomasi80@gmail.com>
2024-11-16 22:24:28 +0100

    rien avancer, et pourtant j'ai essayer...

--- dirty_algo.c
+++ dirty_algo.c
@@ -6,7 +6,7 @@
 /*   By: alexandre <alexandre@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 15:53:10 by atomasi           #+#    #+#             */
-/*   Updated: 2024/11/15 23:07:15 by alexandre        ###   ########.fr       */
+/*   Updated: 2024/11/16 22:23:53 by alexandre        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,21 @@
 
 void	insert_in_b(t_stack **stack_a, t_stack **stack_b)
 {
-	while (stack_b)
+	t_stack	*temp_b;
+
+	temp_b = *stack_b;
+	ft_printf(YELLOW"%d\n"END, (*stack_b)->content); // pour verif un truc
+	while (temp_b)
 	{
-		if ((*stack_a)->content < (*stack_b)->content && (*stack_a)->content > (*stack_b)->next->content)
+		if ((*stack_a)->content < temp_b->content && (*stack_a)->content > temp_b->next->content)
 		{
 			rotate(stack_b);
 			push_b(stack_a, stack_b);
+			read_stack(*stack_a, *stack_b); // pour verif un truc
 			return ;
 		}
-	*stack_b = (*stack_b)->next;
+		temp_b = temp_b->next;
 	}
-	*stack_b = goto_head(*stack_b);
 	if ((*stack_a)->content > (*stack_b)->content)
 	{
 		goto_biggest(stack_b);
@@ -36,7 +40,6 @@ void	insert_in_b(t_stack **stack_a, t_stack **stack_b)
 		push_b(stack_a, stack_b);
 		rotate(stack_b);
 	}
-	return ;
 }
 
 void	dirty_algo(t_stack **stack_a, t_stack **stack_b)
@@ -53,6 +56,7 @@ void	dirty_algo(t_stack **stack_a, t_stack **stack_b)
 	while (temp_a)
 	{
 		insert_in_b(stack_a, stack_b);
+	temp_a = *stack_a; // pas sur du truc
 		temp_a = temp_a->next;
 	}
 	// renvoi dans la stack_a
